8b5de89f43b2921e1301459ed0db6584455cc9dc
Rakibhsaniu <rakibhasanoyoniu@gmail.com>
2023-08-14 23:59:08 +0600

    string codeforces

---
+++ string_constructor.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    // string s("hello world");
+    // string s="hello world";
+    // string s("hello world",5);
+    string s(10,'r');
+    cout<<s<<endl;
+    return 0;
+}
