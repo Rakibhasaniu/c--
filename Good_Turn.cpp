2417de2bdcdd5bb1a9a6c8f2de1b2d6df1332810
Rakibhsaniu <rakibhasanoyoniu@gmail.com>
2023-08-19 09:57:45 +0600

    codechef

---
+++ Good_Turn.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	cin>>x>>y;
+
+	if(x+y>7) cout<<"YES";
+	 else  cout<<"NO";
+	    cout<<endl;
+	
+	}
+	return 0;
+}
---
+++ Squats.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        int s=15;
+        int total = x*s;
+        cout<<total<<endl;
+    }
+    return 0;
+}
