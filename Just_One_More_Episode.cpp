e28fed87b618a4dad6a3fcdeac671b72ee09d95d
Rakibhsaniu <rakibhasanoyoniu@gmail.com>
2023-08-21 06:03:08 +0600

    codechef

---
+++ Just_One_More_Episode.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int time;
+        cin>>time;
+        int show=24;
+        if(time>24){
+            cout<<"Yes"<<endl;
+        } else {
+            cout<<"No"<<endl;
+        }
+    }
+    return 0;
+}
---
+++ Read_Pages.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int page,read,day;
+        cin>>page>>read>>day;
+        int wantRead = read * day;
+        if(page>wantRead){
+            
+        }
+    }
+    return 0;
+}
