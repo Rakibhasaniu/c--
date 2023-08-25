848ed18d42892824f2b078b80df82284dcf23a0d
Rakibhsaniu <rakibhasanoyoniu@gmail.com>
2023-08-25 23:09:07 +0600

    new

--- Subscriptions.cpp
+++ Subscriptions.cpp
@@ -1,17 +1,17 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main ()
-{
-    int t;
-    cin>>t;
-    while(t--) {
-        int n,x;
-        cin>>n>>x;
-        int sub = (n+5)/6;
-        int total = sub * x;
-        cout<<total<<endl;
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main ()
+// {
+//     int t;
+//     cin>>t;
+//     while(t--) {
+//         int n,x;
+//         cin>>n>>x;
+//         int sub = (n+5)/6;
+//         int total = sub * x;
+//         cout<<total<<endl;
         
 
-    }
-    return 0;
-}
+//     }
+//     return 0;
+// }
