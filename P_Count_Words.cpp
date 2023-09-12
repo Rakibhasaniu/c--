d4f6066f67dbf3cfac70f9f1f720be71a5ca244a
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2023-09-12 07:49:29 +0600

    codeforces

--- P_Count_Words.cpp
+++ P_Count_Words.cpp
@@ -1,20 +1,3 @@
-// #include<bits/stdc++.h>
-// using namespace std;
-// int main ()
-// {
-//     string s;
-//     getline(cin,s);
-//     stringstream ss(s);
-//     string result;
-//     int word=0;
-//     while (ss>>result)
-//     {
-//         word++;
-//     }
-    
-//     cout<<word<<endl;
-//     return 0;
-// }
 
 #include<bits/stdc++.h>
 using namespace std;
