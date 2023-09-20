012dd67f2b2ee620cd866a806d84a07a98a40273
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2023-09-20 23:15:02 +0600

    binary search

--- ds & algo/binary_search.cpp
+++ ds & algo/binary_search.cpp
@@ -9,22 +9,14 @@ int main ()
     {
         cin>>a[i];
     }
-    sort(a,a+n);
     while(q--){
         int x;
         cin>>x;
         bool flag=false;
-        int l=0,r=n-1;
-        while(l<=r){
-            int mid = (l+r)/2;
-            if(a[mid]==x){
+        for(int i=0;i<n;i++){
+            if(a[i] == x){
                 flag=true;
                 break;
-            }
-            if(x>a[mid]){
-                l=mid+1;
-            } else {
-                r=mid-1;
             }
         }
         if(flag==true) cout<<"found"<<endl;
