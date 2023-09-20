bd832f59da589d6e6e1f5baf85db8869018d1068
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2023-09-20 20:39:53 +0600

    prefix sum

--- ds & algo/Y_Range_sum_query.cpp
+++ ds & algo/Y_Range_sum_query.cpp
@@ -2,25 +2,29 @@
 using namespace std;
 int main()
 {
-    int n,q;
+    long long n,q;
     cin>>n>>q;
-    int a[n];
+    long long a[n];
     for(int i=0;i<n;i++)
     {
         cin>>a[i];
     }
+    long long pre[n];
+    pre[0]=a[0];
+    for(int i=1;i<n;i++){
+        pre[i]=a[i]+pre[i-1];
+    }
 
     while(q--)
     {
-        int l,r;
+        long long l,r;
         cin>>l>>r;
         l--;
         r--;
-        int sum=0;
-        for(int i=l;i<=r;i++)
-        {
-            sum+=a[i];
-        }
+        long long sum;
+        if(l==0) sum = pre[r];
+        else sum=pre[r]-pre[l-1];
+        
         cout<<sum<<endl;
     }
     return 0;
