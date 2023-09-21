d563d9d96a8c7332d05336b4c3c29ee773a0b41c
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2023-09-22 04:18:35 +0600

    hackerrank prefix array

---
+++ ds & algo/Sorted.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[i];
+        }
+        bool flag = false;
+        for(int i=1;i<n;i++)
+        {
+            if(a[i] < a[i-1]){
+                flag = true;
+                break;
+            }
+        }
+        if(flag == true){
+            cout<<"NO"<<endl;
+        } else {
+            cout<< "YES" << endl;
+            }
+    }
+    return 0;
+}
