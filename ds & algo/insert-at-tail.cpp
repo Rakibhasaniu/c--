4807048876eda8d64199926c7078174c88df0fd8
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2023-09-22 23:27:31 +0600

    insert_at_tail in linkedlist

---
+++ ds & algo/insert-at-tail.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node
+{
+    public:
+        int val;
+        Node * next;
+    Node(int val)
+    {
+        this->val=val;
+        this->next=next;
+    }
+};
+void insert(Node * head,int val)
+{
+    Node * newNode = new Node(val);
+}
+int main ()
+{
+     Node * head = NULL;
+     cout<<"Option 1: Insert at tail"<<endl;
+     int op;
+     cin>>op;
+     if(op==1)
+     {
+
+     }
+    return 0;
+}
