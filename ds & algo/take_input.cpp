2885aef394e193c51caf8627620beee9f5b454a3
Rakibhsaniu <rakibhsanoyoniu@gmail.com>
2023-10-01 21:22:42 +0600

    new file added

---
+++ ds & algo/take_input.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+        int val;
+        Node* next;
+    Node(int val){
+        this->val=val;
+        this->next=NULL;
+    }
+};
+void insert_tail(Node* &head,int v){
+    Node * newNode = new Node(v);
+    if(head == NULL){
+        head = newNode;
+        
+        return;
+    }
+    Node * tmp = head;
+    while(tmp->next != NULL){
+        tmp = tmp -> next ;
+    }
+    tmp->next=newNode;
+   
+}
+void insert_at_head(Node* &head,int val){
+    Node* newNode = new Node(val);
+    newNode->next=head;
+    head=newNode;
+    
+}
+void print(Node * head){
+    Node * tmp = head;
+    while(tmp!=NULL){
+        cout<<tmp->val<<" ";
+        tmp=tmp->next;
+    }
+    cout<<endl<<endl;
+}
+int main ()
+{
+    int val;
+    Node* head = NULL;
+    while (true)
+    {
+        cin >> val;
+        if(val == -1) break;
+        insert_tail(head,val);
+    }
+    print(head);
+    
+    return 0;
+}
