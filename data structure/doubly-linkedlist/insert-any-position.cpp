0be6ebfa267e5f4bbf3afefb2cb961f0b68b5da4
Rakibhsaniu <rakibhasanoyoniu@gmail.com>
2023-10-25 16:28:08 +0600

    doublu linkedlist insert at head & tail

--- data structure/doubly-linkedlist/insert-any-position.cpp
+++ data structure/doubly-linkedlist/insert-any-position.cpp
@@ -42,6 +42,33 @@ void insert_at_position(Node *head,int pos,int data){
     newNode->prev = tmp;
 
 }
+int size(Node *head){
+    Node *tmp = head;
+    int cnt = 0;
+    while(tmp!=NULL){
+        cnt++;
+        tmp = tmp->next;
+    }
+    return cnt;
+}
+void insert_head(Node *&head,Node *&tail,int data){
+    Node *newNode = new Node(data);
+    if(head == NULL){
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    newNode->next = head;
+    head->prev = newNode;
+
+    head = newNode;
+}
+void insert_tail (Node *&tail,int data){
+    Node *newNode = new Node(data);
+    tail->next =newNode;
+    newNode->prev = tail;
+    tail = tail->next;
+}
 int main ()
 {
     Node *head = new Node(10);
@@ -57,8 +84,20 @@ int main ()
     b->next = c;
     c->prev = b;
 
+    int pos,data;
+    cin>>pos>>data;
+    if(pos == 0){
+        insert_head(head,tail,data);
+    } else if(pos == size(head)){
+        insert_tail(tail,data);
+    }
+    else if(pos >= size(head)){
+        cout<<"Invalid"<<endl;
+    } else {
 
     insert_at_position(head,2,100);
+    }
+
     print(head);
     print_reverse(tail);
 
