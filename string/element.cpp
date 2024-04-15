c18194fcba8efc0f5202d44b58d0fd2ca0b0cf78
Rakib Hasan <rakibhasanoyoniu@gmail.com>
2024-04-15 15:27:18 +0600

    capacity in c++ string

---
+++ string/element.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s = "hello world";
+    // cin>>s;
+    cout << s << endl;
+    cout << s.size() << endl;
+    cout << s.length() << endl;
+    cout << s.max_size() << endl;
+    cout << s.substr(3, 5) << endl;
+
+    return 0;
+}
